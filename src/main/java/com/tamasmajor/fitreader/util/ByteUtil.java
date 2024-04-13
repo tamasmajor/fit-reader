0a31c1864f9bd8ac23cf0afa8f744bbc799b25f2
Tamas Major <majortamas8@gmail.com>
2024-04-13 12:22:39 +0200

    extended the ByteUtil to help to check a bit at a specific position in the byte

--- src/main/java/com/tamasmajor/fitreader/util/ByteUtil.java
+++ src/main/java/com/tamasmajor/fitreader/util/ByteUtil.java
@@ -5,6 +5,10 @@ import java.nio.ByteOrder;
 
 public class ByteUtil {
 
+    public static boolean isBitSet(byte b, int position) {
+        return ((b >> position) & 1) == 1;
+    }
+
     public static int asInt(byte[] bytes, ByteOrder byteOrder) {
         if (bytes.length != 4) {
             throw new IllegalArgumentException("Length has to be 4 bytes");
--- src/test/java/com/tamasmajor/fitreader/util/ByteUtilTest.java
+++ src/test/java/com/tamasmajor/fitreader/util/ByteUtilTest.java
@@ -6,10 +6,38 @@ import org.junit.jupiter.api.Test;
 
 import static java.nio.ByteOrder.BIG_ENDIAN;
 import static java.nio.ByteOrder.LITTLE_ENDIAN;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 class ByteUtilTest {
+
+    @Nested
+    class IsBitSetTests {
+
+        @Test
+        void shouldReturnTrueWhenBitIsSet() {
+            assertTrue(ByteUtil.isBitSet((byte) -128, 7));
+            assertTrue(ByteUtil.isBitSet(Byte.parseByte("01000000", 2), 6));
+            assertTrue(ByteUtil.isBitSet(Byte.parseByte("00100000", 2), 5));
+            assertTrue(ByteUtil.isBitSet(Byte.parseByte("00010000", 2), 4));
+            assertTrue(ByteUtil.isBitSet(Byte.parseByte("00001000", 2), 3));
+            assertTrue(ByteUtil.isBitSet(Byte.parseByte("00000100", 2), 2));
+            assertTrue(ByteUtil.isBitSet(Byte.parseByte("00000010", 2), 1));
+            assertTrue(ByteUtil.isBitSet(Byte.parseByte("00000001", 2), 0));
+        }
+
+        @Test
+        void shouldReturnTrueWhenBitIsUnset() {
+            assertFalse(ByteUtil.isBitSet(Byte.parseByte("01111111", 2), 7));
+            assertFalse(ByteUtil.isBitSet(Byte.parseByte("00111111", 2), 6));
+            assertFalse(ByteUtil.isBitSet(Byte.parseByte("01011111", 2), 5));
+            assertFalse(ByteUtil.isBitSet(Byte.parseByte("01101111", 2), 4));
+            assertFalse(ByteUtil.isBitSet(Byte.parseByte("01110111", 2), 3));
+            assertFalse(ByteUtil.isBitSet(Byte.parseByte("01111011", 2), 2));
+            assertFalse(ByteUtil.isBitSet(Byte.parseByte("01111101", 2), 1));
+            assertFalse(ByteUtil.isBitSet(Byte.parseByte("01111110", 2), 0));
+        }
+
+    }
 
     @Nested
     class AsIntTests {
